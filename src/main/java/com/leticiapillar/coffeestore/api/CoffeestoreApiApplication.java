df6947b3eb2fb8caaa4058824afa06c0efe18b3d
Leticia Pillar Lisboa <leticiapillar@gmail.com>
2025-12-13 19:12:21 -0300

    Coffee Store API - Project setup

---
+++ src/main/java/com/leticiapillar/coffeestore/api/CoffeestoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.leticiapillar.coffeestore.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeestoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeestoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leticiapillar/coffeestore/api/CoffeestoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leticiapillar.coffeestore.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeestoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
